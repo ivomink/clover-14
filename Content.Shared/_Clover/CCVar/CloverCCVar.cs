268c2972553ca562a950276988198d01806518eb
ivo <mink4@proton.me>
2026-01-16 00:47:23 +0200

    Rules update + No EORG

---
+++ Content.Shared/_Clover/CCVar/CloverCCVar.cs
@@ -0,0 +1,28 @@
+using Robust.Shared.Configuration;
+using Content.Shared.CCVar.CVarAccess;
+using Content.Shared.Administration;
+
+namespace Content.Shared.Clover.CCVar;
+
+[CVarDefs]
+public sealed partial class CloverCCVars
+{
+    /// <summary>
+    /// Whether the no EORG popup is enabled.
+    /// </summary>
+    public static readonly CVarDef<bool> RoundEndNoEorgPopup =
+    CVarDef.Create("game.round_end_eorg_popup_enabled", true, CVar.SERVER | CVar.REPLICATED);
+
+    /// <summary>
+    /// Skip the no EORG popup.
+    /// </summary>
+    public static readonly CVarDef<bool> SkipRoundEndNoEorgPopup =
+        CVarDef.Create("game.skip_round_end_eorg_popup", false, CVar.CLIENTONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// How long to display the EORG popup for.
+    /// </summary>
+    public static readonly CVarDef<float> RoundEndNoEorgPopupTime =
+        CVarDef.Create("game.round_end_eorg_popup_time", 5f, CVar.SERVER | CVar.REPLICATED);
+
+}
